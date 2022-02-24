83fb003872ce02280298b7d6aab1b1ea84641e9b
GinoMilla <ginomilla@outlook.com>
2022-02-24 16:43:58 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Google_Hashcode2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
