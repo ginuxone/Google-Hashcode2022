0759b559f097e65b0fae3a838645590968c913ab
Linda Frickleton <lindafrickleton@gmail.com>
2022-02-24 20:33:24 +0100

    Score system method

--- Program.cs
+++ Program.cs
@@ -53,6 +53,81 @@ namespace Google_Hashcode2022
         }
         private static void scoreSystem()
         {
+            List<List<Project>> calendar = new List<List<Project>>();
+            List<Project> projects = new List<Project>();
+            List<Project> assingedProjects = new List<Project>();
+            List<Project> endProjects = new List<Project>();
+            List<Contributor> contributors = new List<Contributor>();
+            List<Contributor> freeContributors = new List<Contributor>();
+            freeContributors = contributors;
+            projects.Sort();
+
+            //maxScadenza = punteggio del progetto + bestbefore del progetto. Ossia
+            //i giorni disponibili tali per cui quel progetto ci darà punti.
+            //int maxScadenza = projects.Last().bestbefore + projects.Last().score;
+            int maxScadenza = 50; //provvisorio
+
+            int score = 0;
+
+            for (int day = 0; day < maxScadenza; day++)
+            {
+
+                //aggiornare e controllare se un progetto è finito
+                foreach (Project project in projects)
+                {
+                    if (assingedProjects.Contains(project))
+                        project.duration--;
+                    if (project.duration == 0)
+                    {
+                        endProjects.Add(project);
+                        foreach (Contributor contributor in project.contributors)
+                        {
+                            freeContributors.Add(contributor);
+                            assingedProjects.Remove(project);
+                            int delay = day - project.bestbefore;
+                            if (delay < 0) delay = 0;
+                            score += project.score - delay;
+
+
+                            //migliorare le skills
+                        }
+                    }
+                }
+
+                //assegnare i progetti
+                foreach (Project project in projects)
+                {
+                    List<Skill> soddisfatte = new List<Skill>();
+                    if (!assingedProjects.Contains(project) && !endProjects.Contains(project))
+                    {
+                        foreach (Skill reqSkill in project.skills)
+                        {
+                            foreach (Contributor contributor in contributors)
+                            {
+                                if (freeContributors.Contains(contributor))
+                                {
+                                    foreach (Skill contSkill in contributor.skills)
+                                    {
+                                        if (contSkill.name == reqSkill.name && contSkill.level >= reqSkill.level)
+                                        {
+                                            freeContributors.Remove(contributor);
+                                            project.nContributors--;
+                                            project.contributors.Add(contributor);
+                                            soddisfatte.Add(reqSkill);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (project.nContributors == 0)
+                        {
+                            assingedProjects.Add(project);
+                        }
+
+                        calendar.Add(assingedProjects);
+                    }
+                }
+            }
 
         }
         private static void fileWrite(List<Project> completedProjects)
