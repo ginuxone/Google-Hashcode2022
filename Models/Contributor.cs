ff6b971ad478d1f58ebf53ab26ff0d3eb233c3fb
Andrea <andrea.0382@gmail.com>
2022-02-24 19:16:09 +0100

    added contributor + Project + dictionary

--- Models/Contributor.cs
+++ Models/Contributor.cs
@@ -4,16 +4,16 @@ namespace Google_Hashcode2022
 {
     class Contributor{
         String name;
-        List<string> lista_skill;
+         Dictionary<String, int> lista_skill;
 
-        public Contributor(string n, List<String> lista) {
+        public Contributor(string n,  Dictionary<String, int> lista) {
             this.name = n;
             this.lista_skill= lista;
         }
 
          public Contributor(string n) {
             this.name = n;
-            this.lista_skill= new List<string>();
+            this.lista_skill= new  Dictionary<String, int>();
         }
     }
 }
--- Models/Project.cs
+++ Models/Project.cs
@@ -1,7 +1,27 @@
 using System;
+using System.Collections.Generic;
 namespace Google_Hashcode2022
 {
     class Project  {
-        
+        string name;
+
+        int score;
+
+        int day_to_terminate;
+
+        int number_contributor;
+        int duration;
+
+        Dictionary<String, int> skill_list_required;
+
+        public Project(string name, int score, int day_to_terminate, int number_contributor, int duration,  Dictionary<String, int> skill_list_required) {
+            this.name = name;
+            this.score= score;
+            this.number_contributor= number_contributor;
+            this.duration= duration;
+            this.day_to_terminate= day_to_terminate;
+            this.skill_list_required= skill_list_required;
+        }
+
     }
 }
