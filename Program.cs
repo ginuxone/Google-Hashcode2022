1d6bcca994963e640d2ce4d2faa80efe3e0bed91
GinoMilla <ginomilla@outlook.com>
2022-02-24 18:34:07 +0100

    Fix

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace Google_Hashcode2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        ///Build Models and start prioritizing Data
+        private static void fileParser(bool firstLine)
+        {
+            File.ReadLines("");
+            foreach (var item in File.ReadLines(""))
+            {
+                if (firstLine)
+                {
+                    firstLine = false;
+                    // DurataSimulazione = int.Parse(line.Split(" ")[0]);
+                    // n_Intersezioni = int.Parse(line.Split(" ")[1]);
+                    // n_streets = int.Parse(line.Split(" ")[2]);
+                    // n_macchine = int.Parse(line.Split(" ")[3]);
+                    // score = int.Parse(line.Split(" ")[4]);
+                }
+                else
+                {
+
+                }
+            }
+
+        }
+    }
+}
